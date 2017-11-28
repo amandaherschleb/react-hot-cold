e5ae1fa835d9695c3cc24fd7ee722cc9d2ddce29
Amanda Herschleb <amanda.herschleb@gmail.com>
2017-11-28 11:26:39 -0700

    connected guess form component

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,29 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {makeGuess} from '../actions';
 
 import './guess-form.css';
 
-export default class GuessForm extends React.Component {
-    onGuess(event) {
-        event.preventDefault();
+export function GuessForm(props) {
+  function onGuess(e) {
+    e.preventDefault();
 
-        if (this.props.onGuess) {
-            const value = this.input.value;
-            this.props.onGuess(value);
-        }
-        this.input.value = '';
+    if (props.onGuess) {
+      const value = e.target.value;
+      props.dispatch(makeGuess(value));
     }
 
-    render() {
-        return (
-            <form onSubmit={e => this.onGuess(e)}>
-                <label htmlFor="userGuess">Enter your Guess</label>
-                <input type="text" name="userGuess" id="userGuess"
-                    className="text" maxLength="3" autoComplete="off"
-                    placeholder={Math.round(Math.random() * 100)} required
-                    ref={input => this.input = input} />
-                <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
-            </form>
-        );
-    }
+    target.value = '';
+  }
+
+  return (
+    <form onSubmit={onGuess}>
+      <label htmlFor="userGuess">Enter your Guess</label>
+      <input type="text" name="userGuess" id="userGuess"
+        className="text" maxLength="3" autoComplete="off"
+        placeholder={Math.round(Math.random() * 100)} required />
+      <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+    </form>
+  );
 };
 
+export default connect()(GuessCount);
